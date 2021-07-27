5d645df85601d65f19f7b563fdbb6f926abb45dd
Rajeev Kuruganti <rkuruganti@4circlesllc.com>
2021-07-27 10:42:45 -0400

    initial checkin

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, Dialog, TextField} from '@material-ui/core';
+import { MyDialog } from './MyDialog';
+import { convertCompilerOptionsFromJson } from 'typescript';
+import FormDialog from './FormDialog';
 
 function App() {
+ const  [open, setOpen]= React.useState(false)
+ const [name,setName]= React.useState('')
+ const handleClickOpen = () =>{
+     setOpen(true)
+ }
+ const handleClickClose = () =>{
+  setOpen(false)
+}
+const handleClickSave = () => {
+  console.log("name =  --> " + name)
+  setName(name)
+  handleClickClose()
+  
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+ 
+     
+         Hello World  and {name}
+         <div>
+         <Button onClick={handleClickOpen} color="primary">Open Dialog</Button>
+         {/* <MyDialog open={open}></MyDialog> */}
+
+         <div>
+           <FormDialog open={open} name={name}></FormDialog>
+         {/* <Dialog open={open}>
+         Hello in a dialog
+          <div>
+            <TextField label="Name"  >
+ 
+            </TextField>
+          </div>
+           <Button onClick={handleClickClose} color="primary">
+             Cancel
+           </Button>
+           <Button onClick={handleClickSave} color="primary">
+             Save
+           </Button>
+ 
+         </Dialog> */}
+         </div>
+         </div>
+
       </header>
     </div>
   );
